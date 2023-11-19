2e63870256c8163503b09bc0de79c69060952950
Sniper_TVmc <snipertv59.pro@gmail.com>
2023-11-19 21:51:19 +0100

    Update

---
+++ scripts/indexPageScripts.js
@@ -0,0 +1,22 @@
+// script.js
+
+// Actions spécifiques pour chaque bouton
+function homeButtonClick() {
+    window.location.href = "../index.html";
+}
+
+function projectsButtonClick() {
+    window.location.href = "../pages/projectsPage.html";
+}
+
+function socialNetworksButtonClick() {
+    window.location.href = "../pages/socialNetworksPage.html";
+}
+
+function contactButtonClick() {
+    window.location.href = "../pages/contactPage.html";
+}
+
+function blogButtonClick() {
+    window.location.href = "../pages/blogPage.html";
+}
